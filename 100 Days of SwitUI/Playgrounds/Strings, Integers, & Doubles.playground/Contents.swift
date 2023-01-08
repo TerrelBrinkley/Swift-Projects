4b4adbf893d05d01ebe1a52acff85b54217d4aef
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-01-08 15:16:40 -0500

    String Practice

---
+++ 100 Days of SwitUI/Playgrounds/Strings, Integers, & Doubles.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
