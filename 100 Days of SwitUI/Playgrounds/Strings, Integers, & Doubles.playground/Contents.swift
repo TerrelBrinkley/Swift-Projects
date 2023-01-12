8ba8fbcc3f277ce4397f2a104ce74faf9e7bee7b
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-01-11 21:13:26 -0500

    Understanding Doubles

--- 100 Days of SwitUI/Playgrounds/Strings, Integers, & Doubles.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Strings, Integers, & Doubles.playground/Contents.swift
@@ -54,3 +54,29 @@ let number = 120
 print(number.isMultiple(of: 3))
 
 print(120.isMultiple(of: 3))
+
+// let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+
+// let c = a + b
+// let c = a + Int(b)
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 1
+
+var name = "Nicolas Cage"
+name = "John Travolta"
+
+// not allowed
+// name = 57
+
+var rating = 5.0
+rating *= 2
+
+// CGFloat = Double
