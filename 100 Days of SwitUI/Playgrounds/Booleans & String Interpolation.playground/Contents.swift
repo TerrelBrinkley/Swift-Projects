797ac82853faa62216920e7f97897f4500de1e6a
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-01-18 22:55:47 -0500

    String Interpolation Practice

--- 100 Days of SwitUI/Playgrounds/Booleans & String Interpolation.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Booleans & String Interpolation.playground/Contents.swift
@@ -21,3 +21,27 @@ isAuthenticated = !isAuthenticated
 print(isAuthenticated)
 isAuthenticated = !isAuthenticated
 print(isAuthenticated)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greating = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+
+var luggageCode = "1" + "2" + "3" + "4" + "5"
+
+/** Swift will have to concatenate each string one by one as seen below:
+var luggageCode = "12" + "3" + "4" + "5"
+var luggageCode = "123" + "4" + "5"
+var luggageCode = "1234" + "5"
+var luggageCode = "12345"
+**/
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, My name is \(name) and I'm \(age) years old"
+print(message)
+
+print("5 x 5 is \(5 * 5)")
