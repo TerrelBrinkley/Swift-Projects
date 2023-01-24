f567eace68b31ad2c36870ed4544b32dd93c083e
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-01-23 23:46:21 -0500

    Checkpoint 1 - Completed

--- 100 Days of SwitUI/Playgrounds/Checkpoint 1.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Checkpoint 1.playground/Contents.swift
@@ -1,3 +1,9 @@
 import Cocoa
 
 // Checkpoint 1 - 100 Days of SwiftUI
+// Converting Celsius to Fahrenheit
+
+let celsius = 25
+let fahrenheit = (celsius * 9) / 5 + 32
+
+print("The current temperature is \(celsius)°c, or \(fahrenheit)°f")
