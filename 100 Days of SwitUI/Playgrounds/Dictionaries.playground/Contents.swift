958910734b5e336491475db7b881e6185467333a
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-02-04 23:21:19 -0500

    Dictionaries Practice

--- 100 Days of SwitUI/Playgrounds/Dictionaries.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Dictionaries.playground/Contents.swift
@@ -2,4 +2,14 @@ import Cocoa
 
 // How to store and find data in Dictionaries
 
-let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+// let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+// print(employee2["name"])
+// print(employee2["job"])
+// print(employee2["location"])
