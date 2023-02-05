de37c73a598cb79b230194ef699133a5988b556d
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-02-04 23:39:51 -0500

    Dictionaries Practice

--- 100 Days of SwitUI/Playgrounds/Dictionaries.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Dictionaries.playground/Contents.swift
@@ -10,6 +10,28 @@ let employee2 = [
     "location": "Nashville"
 ]
 
-// print(employee2["name"])
-// print(employee2["job"])
-// print(employee2["location"])
+/*
+- Returning Optionals - The existence of data is optional in Dictionaries just like Arrays
+ 
+print(employee2["name"])
+print(employee2["job"])
+print(employee2["location"])
+*/
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo",
+]
+
+print(olympics[2021, default: "Unknown"])
