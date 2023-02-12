a7e72318aa1d87ef17bc1dc29e334d8d2b1a0e63
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-02-11 21:30:30 -0500

    Array Practice

--- 100 Days of SwitUI/Playgrounds/Arrays.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Arrays.playground/Contents.swift
@@ -22,6 +22,8 @@ let firstNumber = numbers[0]
 
 // Not Allowed > let notAllowed = firstBeatle + firstNumber
 
+// Declaring Array to accept only 'Int" Values
+
 var scores = Array<Int>()
 scores.append(100)
 scores.append(80)
@@ -31,14 +33,14 @@ print(scores[1])
 // Declaring Array to accept only String Values
 
 var albums1 = Array<String>()
-albums1.append("Forklore")
+albums1.append("Folklore")
 albums1.append("Fearless")
 albums1.append("Red")
 
 // Declaring Array using simple bracket method - Only accepting String Values
 
 var albums2 = [String]()
-albums2.append("Forklore")
+albums2.append("Folklore")
 albums2.append("Fearless")
 albums2.append("Red")
 
