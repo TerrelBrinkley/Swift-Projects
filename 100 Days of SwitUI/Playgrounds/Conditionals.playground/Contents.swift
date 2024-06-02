c2847dcca0e9ac9110c7ac69e86365df8db6407c
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2024-06-02 00:48:12 -0400

    3rd Example Added

--- 100 Days of SwitUI/Playgrounds/Conditionals.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Conditionals.playground/Contents.swift
@@ -13,3 +13,41 @@ let score = 85
 if score > 80 {
     print("Great job!")
 }
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print ("Sorry, you failed the test.")
+}
+
+if age <= 18 {
+    print("You'e eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+// Make an array of 3 numbers
+var numbers = [1,2,3]
+
+// Add a 4th
+numbers.append(4)
+
+// If we have over 3 items
+if numbers.count > 3 {
+    // Remove the oldest number
+    numbers.remove(at: 0)
+}
+
+// Display the result
+print(numbers)
