fc93df26ca5fc4d86cd7fab4fe4bd4cf708e029a
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2024-06-01 23:52:16 -0400

    Condition added

--- 100 Days of SwitUI/Playgrounds/Conditionals.playground/Contents.swift
+++ 100 Days of SwitUI/Playgrounds/Conditionals.playground/Contents.swift
@@ -1,2 +1,15 @@
 import UIKit
 
+/*
+if someConditon {
+    print("Do something")
+    print("Do something else")
+    print("Do a third thing")
+}
+*/
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
